f8b2e059d3708ff2d8da5934ea57b1e1b81b40fd
HarizahSyawal <37772347+HarizahSyawal@users.noreply.github.com>
2023-06-13 23:16:12 +0700

    Update
    - Remove action bar
    - splashscreen

--- app/src/main/java/com/example/submissionapp/ui/splash/SplashActivity.java
+++ app/src/main/java/com/example/submissionapp/ui/splash/SplashActivity.java
@@ -2,45 +2,33 @@ package com.example.submissionapp.ui.splash;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.submissionapp.MainActivity;
 import com.example.submissionapp.R;
-import com.example.submissionapp.viewmodel.SplashViewModel;
-
 
 public class SplashActivity extends AppCompatActivity {
 
-    private SplashViewModel splashViewModel;
+    // Splash screen duration in milliseconds
+    private static final int SPLASH_DURATION = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set the content view for the splash screen activity
         setContentView(R.layout.activity_splash);
 
-        splashViewModel = new ViewModelProvider(this).get(SplashViewModel.class);
+        // Delayed execution to move to the next activity after the splash screen duration
+        new Handler().postDelayed(() -> {
+            // Start the next activity after the splash screen duration
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
 
-        // Observe any data or events from the ViewModel
-        splashViewModel.getNavigationEvent().observe(this, new Observer<Boolean>() {
-            @Override
-            public void onChanged(Boolean navigateToNextScreen) {
-                if (navigateToNextScreen) {
-                    navigateToNextActivity();
-                }
-            }
-        });
-
-        splashViewModel.startSplashScreenTimer();
-    }
-
-    private void navigateToNextActivity() {
-        // Navigate to the next activity or perform any necessary actions
-        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-        startActivity(intent);
-        finish();
+            // Finish the splash screen activity
+            finish();
+        }, SPLASH_DURATION);
     }
 }
-
