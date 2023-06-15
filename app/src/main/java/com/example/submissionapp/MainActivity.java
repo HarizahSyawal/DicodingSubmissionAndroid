e3f7420fdfaa19d27ca537f6a61125b4469c7820
HarizahSyawal <37772347+HarizahSyawal@users.noreply.github.com>
2023-06-16 00:15:09 +0700

    Update
    - Slicing header and slider

--- app/src/main/java/com/example/submissionapp/MainActivity.java
+++ app/src/main/java/com/example/submissionapp/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.submissionapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
---
+++ app/src/main/java/com/example/submissionapp/SlideAdapter.java
@@ -0,0 +1,25 @@
+package com.example.submissionapp;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.makeramen.roundedimageview.RoundedImageView;
+
+public class SlideAdapter {
+
+//    class SlideViewHolder extends RecyclerView{
+//        private RoundedImageView imageView;
+//
+//        public SlideViewHolder(@NonNull View itemView) {
+//            super(itemView);
+//            imageView = itemView.findViewById(R.id.imageSlide);
+//        }
+//
+//        void setImage(SlideItem slideItem){
+//            imageView.setImageResource(slideItem.getImage());
+//        }
+//    }
+
+}
